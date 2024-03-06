1090378d1ebbb93428ab73186a2039e8a470fdd5
kyad <53517611+kyad@users.noreply.github.com>
2024-03-07 00:36:19 +0900

    Update keywords

--- nadesiko-mode.el
+++ nadesiko-mode.el
@@ -31,6 +31,18 @@
 
 (add-to-list 'auto-mode-alist '("\\.nako3$" . nadesiko-mode))
 
+(font-lock-add-keywords 'nadesiko-mode
+                        '(("もし" . font-lock-keyword-face)
+                          ("ならば" . font-lock-keyword-face)
+                          ("違えば" . font-lock-keyword-face)
+                          ("ここまで" . font-lock-keyword-face)
+                          ("代入" . font-lock-keyword-face)
+                          ("定" . font-lock-keyword-face)
+                          ("戻" . font-lock-keyword-face)
+                          ("●" . font-lock-function-name-face)
+                          ("はい" . font-lock-constant-face)
+                          ("いいえ" . font-lock-constant-face)))
+
 (provide 'nadesiko-mode)
 
 ;;; nadesiko-mode.el ends here
